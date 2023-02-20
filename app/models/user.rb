366a3f9e9149ec254b96a5c864e4d7d113733d36
luccamicale <luccamicale2015@gmail.com>
2023-02-20 16:56:06 -0300

    create asociations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :groups
+  has_many :payments
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
