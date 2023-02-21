0056e2bec1c858555442938b2e5e59cc9503f367
luccamicale <luccamicale2015@gmail.com>
2023-02-21 14:39:50 -0300

    modified models and views

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :payments
+  has_many :payments, dependent: :destroy
 
   validates :name, presence: true
 end
